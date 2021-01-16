27120b944d3ac89e144ae0cdc56436c976607de5
Fadly <73419981+fngadiyo@users.noreply.github.com>
2021-01-16 23:43:27 +0700

    Add files via upload

---
+++ todoList.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const prompt = require('prompt-sync')()
+
+const command = prompt('write your command: ')
+
+const checkCommand = () => {
+    if (command === 'create') {
+        createTodoFile()
+    }
+    if (command === 'add') {
+        addTodo()
+    }
+    if (command === 'delete') {
+        deleteTodoFile()
+    }
+    if (command === 'view') {
+        viewTodo()
+    }
+}
+
+const createTodoFile = () => {
+    fs.appendFileSync('todo.txt')
+}
+
+const addTodo = () => {
+    const todo = prompt('what will you do: ')
+    fs.appendFileSync('todo.txt', todo)
+}
+
+const deleteTodoFile = () => {
+    fs.unlinkSync('todo.txt')
+}
+
+const viewTodo = () => {
+    console.log(fs.readFileSync('todo.txt').toString())
+}
+
+checkCommand()
